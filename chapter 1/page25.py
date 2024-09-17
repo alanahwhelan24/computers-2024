ab42e4ddf92988b99c6689a11dd1a9697278dfe1
alanahwhelan24 <20.alanah.whelan@msltcastlegregory.ie>
2024-09-17 13:09:24 -0200

    Create page25.py

---
+++ chapter 1/page25.py
@@ -0,0 +1,52 @@
+#name = input(" Enter your name: ")
+#print(name)
+#name=name.upper()
+#print(name)
+#name=name.lower()
+#print(name)
+#name=name.capatalized()
+#print(name)
+#lengthName= len(name)
+#print(lengthName)
+
+#print()
+#string1 = 
+#string1 =string1.upper()
+#print(string1)
+
+#string1 = "HELLO"
+#string1 = string1.lower()
+#print(string1)
+
+#string1 = "hELLo"
+#string1 = string1.capitalize()
+#print(string1)
+
+#string1 = "Hello"
+#length = len(string1)
+#print(length)
+#print(type(length))
+
+#task1
+#print( "what is your favourite artists name:")
+#visitor= input()
+#print( "i love them aswell!")
+#print()
+
+#task4
+number= (int(input(" enter a 5 digit number:")))
+mins = 60
+print (number//mins)
+
+#task5
+letter= input("insert 5 charecters")
+ascii= [ord(char) for char in letter]
+print(ascii)
+
+#task7
+fish= $4.50
+chips=m $2.80
+
+
+
+
